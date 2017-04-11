831f00c1fa3b95e32a07d45784526dc56fe6ee32
Andrew <andrew.scancari@concil.com.br>
2017-04-11 13:21:32 -0300

    Configuracao inicial

---
+++ config/routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+module.exports = function(server) {
+  // API routes
+  const router = express.Router();
+  server.use('/api', router);
+
+  router.route('/teste').get(function(req, res, next) {
+    res.send('funcionou!');
+  });
+
+  const billingCycleService = require('../api/billingCycle/billingCycleService');
+  billingCycleService.register(router, '/billingCycles');
+}
